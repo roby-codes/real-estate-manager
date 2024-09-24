0be29535ffc383f76c165f6b991a59cbe85e4ada
Roby Codes <robert.cfx@proton.me>
2024-09-25 00:32:40 +0200

    feat(database): Created root drizzle orm files and config.

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,12 +1,15 @@
 import postgres from "postgres";
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 
 import { env } from "@/env";
 
 const postgresClient = postgres(env.POSTGRES_URI, { max: 1 });
-const drizzleClient = drizzle(postgresClient);
+const migrationClient = drizzle(postgresClient);
 
-migrate(drizzleClient, {
-  migrationsFolder: "./migrations",
+migrate(migrationClient, {
+  migrationsFolder: "./",
 });
+
+postgresClient.end();

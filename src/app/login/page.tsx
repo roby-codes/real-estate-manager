1d4a9dcc756d7a7772926248b9f34be674758313
Roby Codes <robert.cfx@proton.me>
2024-09-26 23:58:31 +0200

    feat(page): Created login page.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { LoginForm } from "@/components/login-form";
+
+const LoginPage = () => (
+  <main>
+    <section className="min-h-screen flex justify-center items-center">
+      <div className="container max-w-sm">
+        <LoginForm />
+      </div>
+    </section>
+  </main>
+);
+
+export default LoginPage;

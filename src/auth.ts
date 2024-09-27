64e2c166913c11bc59998b4b475901ed6ad94a64
Roby Codes <robert.cfx@proton.me>
2024-09-27 13:44:54 +0200

    revert(auth): Reverted auth files to default.

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Credentials],
 });

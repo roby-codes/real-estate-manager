57adde56c831a17d4deae7db725a68cdd1e1fbd1
Roby Codes <robert.cfx@proton.me>
2024-09-28 01:18:59 +0200

    feat(auth): Created auth config file.

---
+++ src/config/auth.config.ts
@@ -0,0 +1,21 @@
+import { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { getAndValidateUser } from "@/data/user";
+import { LoginSchema } from "@/schemas/login-schema";
+
+export const authConfig = {
+  providers: [
+    Credentials({
+      authorize: async (credentials) => {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (!validatedFields.success) return null;
+
+        return await getAndValidateUser(validatedFields.data);
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+} satisfies NextAuthConfig;
